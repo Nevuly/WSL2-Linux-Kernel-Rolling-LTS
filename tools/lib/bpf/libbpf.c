4fa618c81da7e569d1a04113852060668da52be4
Yang Jeong Hun <onyxclover9931@gmail.com>
2026-05-15 15:00:59 +0900

    Revert "libbpf: Fix -Wdiscarded-qualifiers under C23"
     * This reverts commit 5696dfba10942d305bcc97804b9831045d2c83eb.
    
    Signed-off-by: Yang Jeong Hun <onyxclover9931@gmail.com>

--- tools/lib/bpf/libbpf.c
+++ tools/lib/bpf/libbpf.c
@@ -11247,7 +11247,7 @@ static int resolve_full_path(const char *file, char *result, size_t result_sz)
 		if (!search_paths[i])
 			continue;
 		for (s = search_paths[i]; s != NULL; s = strchr(s, ':')) {
-			const char *next_path;
+			char *next_path;
 			int seg_len;
 
 			if (s[0] == ':')
